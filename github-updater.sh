90b83ca6c45a3be0cdcc015208979885e3193e45
duxiaofeng-github <297951292@qq.com>
2019-08-08 00:10:10 +0800

    fix pub key

--- github-updater.sh
+++ github-updater.sh
@@ -10,11 +10,12 @@ function initKey() {
 
     if [[ ! -f $privateKeyPath ]] && [[ $adk != "" ]]; then
         printf -- "$adk" >> $privateKeyPath
-        # echo "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAD96gADB8vfL8hUVLYSxcUuGF1bwl9LyunFcuFT3bMZMLcaqmCjhm1Y0trg/zolbLpjeKohojUkFfx23XmxbgBDEADoQ0wGBxNfPFXLJj6Q0SJ0St1DVsJchk8iIkXXmPM8B45oQY+fVnBc3HC2upLB15OQcLbZMANBPyPN4bHb98HuQQ== duxiaofeng@duxiaofengdeMacBook-Pro.local" > $publicKeyPath
+        echo "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAD96gADB8vfL8hUVLYSxcUuGF1bwl9LyunFcuFT3bMZMLcaqmCjhm1Y0trg/zolbLpjeKohojUkFfx23XmxbgBDEADoQ0wGBxNfPFXLJj6Q0SJ0St1DVsJchk8iIkXXmPM8B45oQY+fVnBc3HC2upLB15OQcLbZMANBPyPN4bHb98HuQQ== duxiaofeng@duxiaofengdeMacBook-Pro.local" > $publicKeyPath
         chmod og-rwx $privateKeyPath
-        # chmod og-rwx $publicKeyPath
-        # ls -l $keyDir
-        cat $privateKeyPath | tail -n 2
+        chmod og-rwx $publicKeyPath
+        ls -l $keyDir
+        cat $privateKeyPath
+        cat $publicKeyPath
         printf "Host github.com\n  IdentityFile $privateKeyPath" >> $sshConfigPath
     fi
 }
